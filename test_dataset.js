ed50d5d557dc3545e6a5f7e3d09dc894bd2bd44e
qrizs <137526876+qrizs@users.noreply.github.com>
2023-07-15 23:26:30 +0800

    Update test_dataset.js

--- test_dataset.js
+++ test_dataset.js
@@ -8,17 +8,21 @@ const measuresList = [
 
 //hard-coded for now, supposed to be retrieved from model list stored somewhere in the future
 const dimListTemp = {
-    'Account': {fieldName: 'account', desc: 'Chart of Accounts', type: 'Account', table: {name: 'tabaccount2', id: 'name', desc: 'account_name', parent: {parent_account: 'Parent Account', old_parent: 'Old Parent'}}},
+    'Account': {fieldName: 'account', desc: 'Chart of Accounts', type: 'Account', table: {name: 'tabaccount', id: 'name', desc: 'account_name', parent: {parent_account: 'Parent Account', old_parent: 'Old Parent'}}},
     'Company': {fieldName: 'company', desc: 'Company', type: 'Entity', table: {name: 'tabcompany', id: 'name', desc: 'company_name', parent: {}}},
+    'CostCenter': {fieldName: 'cost_center', desc: 'Cost center', type: 'Entity', table: {name: '`tabcost center`', id: 'name', desc: 'cost_center_name', parent: {parent: 'Parent'}}},
+    'Currency': {fieldName: 'account_currency', desc: 'Currency', type: 'Currency', table: {name: 'tabcurrency', id: 'name', desc: 'company_name', parent: {}}},
+    'Customer': {fieldName: 'customer', desc: 'Customer name', type: 'Person', table: {name: 'tabcustomer', id: 'name', desc: 'currency_name', parent: {}}},    
+    'Department': {fieldName: 'department', desc: 'Department', type: 'Other', table: {name: 'tabdepartment', id: 'name', desc: 'department_name', parent: {}}},    
     'Employee': {fieldName: 'employee', desc: 'Employee name', type: 'Person', table: {name: 'tabemployee', id: 'name', desc: 'employee_name' , parent: {}}},
-    'Department': {fieldName: 'department', desc: 'Department', type: 'Other', table: {name: 'tabdepartment', id: 'name', desc: 'department_name', parent: {}}},
-    'Customer': {fieldName: 'customer', desc: 'Customer name', type: 'Person', table: {name: 'tabcustomer', id: 'name', desc: 'customer_name', parent: {}}},
     'Patient': {fieldName: 'patient', desc: 'Patient name', type: 'Person', table: {name: 'tabpatient', id: 'name', desc: 'patient_name', parent: {}}},
+    'PostingDate': {fieldName: 'posting_date', desc: 'Posting date', type: 'Date', table: {name: 'cstdate', id: 'id', desc: 'date_name', parent: {parent: 'Parent'}}},
     'Supplier': {fieldName: 'supplier', desc: 'Supplier', type: 'Other', table: {name: 'tabsupplier', id: 'name', desc: 'supplier_name', parent: {}}},
-    'Movement': {fieldName: '', desc: 'Movement', type: 'Movement', table: {name: 'zmovement', id: 'name', desc: 'description', parent: {parenth1: 'Hier1'}}},
-    'Time': { fieldName: 'posting_date', desc: 'Time', type: 'Time', table: {name: 'ztime', id: 'name', desc: 'description', parent: {parenth1: 'Hier1'}} },
-    'Balance': { fieldName: '', desc: 'Balance', type: 'Other', table: {name: 'zbalance', id: 'name', desc: 'description', parent: {parenth1: 'Hier1'}}},
-    'Measures': { fieldName: '', desc: 'Measures', type: 'Measures', members: measuresList }
+    'SalesInvoice': {fieldName: 'sales_invoice', desc: 'Sales Invoice', type: 'Other', table: {name: '`tabsales invoice`', id: 'name', desc: 'name', parent: {}}}
+    //'Movement': {fieldName: '', desc: 'Movement', type: 'Movement', table: {name: 'zmovement', id: 'name', desc: 'description', parent: {parenth1: 'Hier1'}}},
+    //'Time': { fieldName: 'posting_date', desc: 'Time', type: 'Time', table: {name: 'ztime', id: 'name', desc: 'description', parent: {parenth1: 'Hier1'}} },
+    //'Balance': { fieldName: '', desc: 'Balance', type: 'Other', table: {name: 'zbalance', id: 'name', desc: 'description', parent: {parenth1: 'Hier1'}}},
+    //'Measures': { fieldName: '', desc: 'Measures', type: 'Measures', members: measuresList }
 };
 
 
